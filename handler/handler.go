37d9efbc42e4071b34e0ce3ebd4e0723660fd733
bardenha <bardenha@gmail.com>
2022-02-11 23:18:57 +0000

    feat: mapHandler

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,24 @@
 package handler
 
 import (
-	"fmt"
+	"net/http"
 )
 
-func Test() {
-	fmt.Println("test")
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		path := request.URL.Path
+
+		if destination, ok := pathsToUrls[path]; ok {
+			http.Redirect(writer, request, destination, http.StatusFound)
+			return
+		}
+
+		fallback.ServeHTTP(writer, request)
+	}
 }
--- main.go
+++ main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/bardenHa/urlshortener/handler"
 )
 
-func main() {
-	fmt.Println(":)")
+var pathsToUrls map[string]string = map[string]string{
+	"/ggle":  "https://www.google.co.uk",
+	"/ytube": "https://www.youtube.com",
+}
 
-	handler.Test()
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", welcome)
+
+	mapHandler := handler.MapHandler(pathsToUrls, mux)
+
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", mapHandler)
+}
+
+func welcome(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(writer, "welcome")
 }
