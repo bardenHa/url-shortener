692d25058d60caf3505bde2f93d1c790d15a4e0a
bardenha <bardenha@gmail.com>
2022-02-14 22:15:21 +0000

    feat: YAML handler test

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -2,22 +2,25 @@ package handler
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestParseYaml(t *testing.T) {
-	input := `
+var yml = `
 - path: /twtr
   url: https://twitter.com
 - path: /me
   url: https://github.com/bardenHa
 `
+
+func TestParseYaml(t *testing.T) {
 	expectedOutput := []pathUrl{
 		{Path: "/twtr", URL: "https://twitter.com"},
 		{Path: "/me", URL: "https://github.com/bardenHa"},
 	}
 
-	output, err := parseYaml([]byte(input))
+	output, err := parseYaml([]byte(yml))
 
 	if err != nil {
 		t.Errorf("Expected no error but got: %v", err)
@@ -54,5 +57,39 @@ func TestBuildMap(t *testing.T) {
 
 	if url != "https://twitter.com" {
 		t.Errorf("Incorrect url returned. Got: %q, Expected: %q", url, "https://twitter.com")
+	}
+}
+
+func TestYAMLHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/twtr", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, world!")
+	})
+
+	yamlHandler, err := YAMLHandler([]byte(yml), mux)
+
+	if err != nil {
+		t.Errorf("Expected no error but got: %v", err)
+	}
+
+	handler := http.HandlerFunc(yamlHandler)
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != 302 {
+		t.Errorf("Incorrect response code. Got: %v, Expected: %v", rr.Code, 302)
+	}
+
+	fmt.Println(rr.Header().Get("Location"))
+
+	if rr.Header().Get("Location") != "https://twitter.com" {
+		t.Errorf("Incorrect response body. Got: %q, Expected: %q", rr.Header().Get("Location"), "https://twitter.com")
 	}
 }
